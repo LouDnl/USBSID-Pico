9819c3ed602275abfcefc795d7c7bc114064d72e
loudnl <LouDnl@users.noreply.github.com>
2024-12-20 17:29:56 +0100

    Add util.c

---
+++ src/util.c
@@ -0,0 +1,38 @@
+/*
+ * USBSID-Pico is a RPi Pico (RP2040) based board for interfacing one or two
+ * MOS SID chips and/or hardware SID emulators over (WEB)USB with your computer,
+ * phone or ASID supporting player
+ *
+ * util.c
+ * This file is part of USBSID-Pico (https://github.com/LouDnl/USBSID-Pico)
+ * File author: LouD
+ *
+ * Copyright (c) 2024 LouD
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* Pico libs */
+#include <stdlib.h>
+#include "pico/stdlib.h"
+
+int randval(int min, int max)
+{
+  return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
