6dba1af62f7d8473f5970af82b104e5638cd3fed
loudnl <LouDnl@users.noreply.github.com>
2025-01-12 19:10:40 +0100

    Update config-tool

---
+++ examples/config-tool/cfg_skpico.h
@@ -0,0 +1,71 @@
+/*
+ * USBSID-Pico is a RPi Pico (RP2040) based board for interfacing one or two
+ * MOS SID chips and/or hardware SID emulators over (WEB)USB with your computer,
+ * phone or ASID supporting player
+ *
+ * cfg_skpico.h
+ * This file is part of USBSID-Pico (https://github.com/LouDnl/USBSID-Pico)
+ * File author: LouD
+ *
+ * Copyright (c) 2024-2025 LouD
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+const uint8_t init_configmode[3] = { 0x0, 0x1F, 0xFF };       /* Inits and extends config mode */
+const uint8_t config_unknown[3] = { 0x0, 0x1D, 0xFA };        /* Unknown configuration option */
+const uint8_t config_exit[3] = { 0x0, 0x1D, 0xFB };           /* Exit config mode */
+const uint8_t config_update[3] = { 0x0, 0x1D, 0xFE };         /* Update configuration */
+const uint8_t config_writeupdate[3] = { 0x0, 0x1D, 0xFF };    /* Update and write configuration */
+const uint8_t config_write[3] = { 0x0, 0x1D, 0x0 };           /* Write complete config to memory */
+const uint8_t start_config_write[3] = { 0x0, 0x1E, 0x0 };     /* Write single value to config */
+const uint8_t set_dac_mode_mono8[3] = { 0x0, 0x1F, 0xFC };    /* Set DAC mode to mono */
+const uint8_t set_dac_mode_stereo8[3] = { 0x0, 0x1F, 0xFB };  /* Set DAC mode to stereo */
+
+unsigned char * config_names[/* 64 */] = {
+  "CFG_SID1_TYPE",       //  0   //  1  // 0 ... 3  // 6581, 8580, 8580+digiboost, none
+  "CFG_SID1_DIGIBOOST",  //  1   // 12  // 0 ... 15, 0x0 ... 0xF
+  "CFG_REGISTER_READ",   //  2   //  1  // Always 1
+  "CFG_SID1_VOLUME",     //  3   // 14  // 0 ... 14, 0x0 ... 0xE
+  "","","","",           //  4 -> 7
+  "CFG_SID2_TYPE",       //  8   //  3  // 0 ... 4  // 6581, 8580, 8580+digiboost, none, FMopl
+  "CFG_SID2_DIGIBOOST",  //  9   // 12  // 0 ... 15, 0x0 ... 0xF
+  "CFG_SID2_ADDRESS",    // 10   //  0  // 0 ... 5  // sidFlags{ CS, A5, A8, A5|A8, A8, A8 }
+  "CFG_SID2_VOLUME",     // 11   // 14  // 0 ... 14, 0x0 ... 0xE
+  "CFG_SID_PANNING",     // 12   //  5  // 0 ... 14, 0x0 ... 0xE
+  "","","","","","",     // 13 -> 18
+  "","","","","","",     // 19 -> 24
+  "","","","","","",     // 25 -> 30
+  "","","","","","",     // 31 -> 36
+  "","","","","","",     // 37 -> 42
+  "","","","","","",     // 43 -> 48
+  "","","","","","",     // 49 -> 54
+  "","",                 // 55 -> 56
+  "CFG_TRIGGER",         // 57   //  0  // Paddles
+  "CFG_SID_BALANCE",     // 58   //  7  // 0 ... 14, 0x0 ... 0xE
+  "CFG_CLOCKSPEED",      // 59   //  0  // 0 ... 2  // c64clock{ 985248, 1022727, 1023440 }
+  "CFG_POT_FILTER",      // 60   // 16  // Paddles
+  "CFG_DIGIDETECT",      // 61   //  0  // 0 ... 1
+  "","",                 // 62 -> 63
+};
+
+unsigned char * sid_types[] = {
+  "6581", "8580", "8580+digiboost", "none", "FMopl"
+};
+unsigned char * sid2_address[] = {
+  "CS", "A5", "A8", "A5|A8", "A8", "A8"
+};
+unsigned char * clock_speed[] = {
+  "985248", "1022727", "1023440"
+};
---
+++ examples/config-tool/cfg_usbsid.h
@@ -0,0 +1,198 @@
+/*
+ * USBSID-Pico is a RPi Pico (RP2040) based board for interfacing one or two
+ * MOS SID chips and/or hardware SID emulators over (WEB)USB with your computer,
+ * phone or ASID supporting player
+ *
+ * cfg_usbsid.h
+ * This file is part of USBSID-Pico (https://github.com/LouDnl/USBSID-Pico)
+ * File author: LouD
+ *
+ * Copyright (c) 2024-2025 LouD
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* libusb variables */
+#define VENDOR_ID      0xCAFE
+#define PRODUCT_ID     0x4011
+#define ACM_CTRL_DTR   0x01
+#define ACM_CTRL_RTS   0x02
+
+uint8_t read_data[1];
+uint8_t read_data_max[64];
+uint8_t read_data_uber[128];
+uint8_t read_buffer[3]  = { 0x1, 0x0, 0x0 };
+uint8_t write_buffer[3]  = { 0x0, 0x0, 0x0 };
+uint8_t command_buffer[3]  = { 0x0, 0x0, 0x0 };
+uint8_t config_buffer[5]  = { 0x0, 0x0, 0x0, 0x0, 0x0 };
+
+/* usbsid-pico variables */
+enum
+{
+  WRITE = 0,      /* 0 */
+  READ,           /* 1 */
+  PAUSE,          /* 2 */
+  RESET_SID,      /* 3 */
+  DISABLE_SID,    /* 4 */
+  ENABLE_SID,     /* 5 */
+  CLEAR_BUS,      /* 6 */
+  RESET_MCU,      /* 7 */
+  BOOTLOADER,     /* 8 */
+};
+enum
+{
+  RESET_USBSID     = 0x20,
+
+  READ_CONFIG      = 0x30,
+  APPLY_CONFIG     = 0x31,
+  STORE_CONFIG     = 0x32,
+  SAVE_CONFIG      = 0x33,
+  SAVE_NORESET     = 0x34,
+  RESET_CONFIG     = 0x35,
+
+  SINGLE_SID       = 0x40,
+  DUAL_SID         = 0x41,
+  QUAD_SID         = 0x42,
+  TRIPLE_SID       = 0x43,
+
+  SET_CLOCK        = 0x50,
+  DETECT_SIDS      = 0x51,
+  TEST_ALLSIDS     = 0x52,
+  TEST_SID1        = 0x53,
+  TEST_SID2        = 0x54,
+  TEST_SID3        = 0x55,
+  TEST_SID4        = 0x56,
+
+  LOAD_MIDI_STATE  = 0x60,
+  SAVE_MIDI_STATE  = 0x61,
+  RESET_MIDI_STATE = 0x63,
+
+  USBSID_VERSION   = 0x80,
+
+  TEST_FN          = 0x99,  /* TODO: Remove before v1 release */
+};
+typedef enum {
+    CLOCK_DEFAULT  = 1000000,  /* @125MHz clock ~ 125000000 / 62,5f = 2000000 / 2 = 1000000 == 1.00MHz @ 125MHz and 1.06MHz @ 133MHz */
+    CLOCK_PAL      = 985248,   /* @125MHz clock ~ 125000000 / 63,435804995f = 1970496,000009025 / 2 = 985248,000004513 */
+    CLOCK_NTSC     = 1022727,  /* @125MHz clock ~ 125000000 / 61,111127407f = 2045454,000013782 / 2 = 1022727,000006891 */
+    CLOCK_DREAN    = 1023440   /* @125MHz clock ~ 125000000 / 61,068553115f = 2046879,999999489 / 2 = 1023439,999999745 */
+} clock_rates;
+enum config_clockrates
+{
+    DEFAULT = CLOCK_DEFAULT,  /* 0 */
+    PAL     = CLOCK_PAL,      /* 1 */
+    NTSC    = CLOCK_NTSC,     /* 2 */
+    DREAN   = CLOCK_DREAN,    /* 3 */
+};
+
+const char * enabled[] = {"Disabled", "Enabled"};
+const char * intext[] = { "Internal", "External" };
+const char * onoff[] = {"Off", "On"};
+const char * truefalse[] = {"False", "True"};
+const char * socket[] = { "Single SID", "Dual SID" };
+const char * chiptypes[] = {"Real", "Clone"};
+const char * sidtypes[] = {"Unknown", "N/A", "MOS8580", "MOS6581", "FMopl"};
+const char * clonetypes[] = { "Disabled", "Other", "SKPico", "ARMSID", "FPGASID", "RedipSID" };
+
+/* USBSID-Pico config struct */
+typedef struct Config {
+  bool external_clock : 1;     /* enable / disable external oscillator */
+  uint32_t clock_rate;         /* clock speed identifier */
+  struct
+  {
+    bool    enabled : 1;       /* enable / disable this socket */
+    bool    dualsid : 1;       /* enable / disable dual SID support for this socket (requires clone) */
+    uint8_t chiptype;          /* 0 = real, 1 = clone */
+    uint8_t clonetype;         /* 0 = disabled, 1 = SKPico, 2 = ARMSID, 3 = FPGASID, 4 = other */
+    uint8_t sid1type;          /* 0 = unknown, 1 = n/a, 2 = MOS8085, 3 = MOS6581 */
+    uint8_t sid2type;          /* 0 = unknown, 1 = FMopl, 2 = MOS8085, 3 = MOS6581 */
+  } socketOne;                 /* 1 */
+  struct {
+    bool    enabled : 1;       /* enable / disable this socket */
+    bool    dualsid : 1;       /* enable / disable dual SID support for this socket (requires clone) */
+    bool    act_as_one : 1;    /* act as socket 1 */
+    uint8_t chiptype;          /* 0 = real, 1 = clone */
+    uint8_t clonetype;         /* 0 = disabled, 1 = SKPico, 2 = ARMSID, 3 = FPGASID, 4 = other */
+    uint8_t sid1type;          /* 0 = unknown, 1 = n/a, 2 = MOS8085, 3 = MOS6581 */
+    uint8_t sid2type;          /* 0 = unknown, 1 = FMopl, 2 = MOS8085, 3 = MOS6581 */
+  } socketTwo;                 /* 2 */
+  struct {
+    bool enabled : 1;
+    bool idle_breathe : 1;
+  } LED;                       /* 3 */
+  struct {
+    bool    enabled : 1;
+    bool    idle_breathe : 1;
+    uint8_t brightness;
+    int     sid_to_use;         /* 0 = sid 1&2, 1...4 = sid 1 ... sid 4 */
+  } RGBLED;                     /* 4 */
+  struct {
+    bool enabled : 1;
+  } Cdc;                        /* 5 */
+  struct {
+    bool enabled : 1;
+  } WebUSB;                     /* 6 */
+  struct {
+    bool enabled : 1;
+  } Asid;                       /* 7 */
+  struct {
+    bool enabled : 1;
+    uint8_t sid_states[4][32];  /* Stores states of each SID ~ 4 sids max */
+  } Midi;                       /* 8 */
+} Config;
+
+#define USBSID_DEFAULT_CONFIG_INIT { \
+  .external_clock = false, \
+  .clock_rate = 1000000, \
+  .socketOne = { \
+    .enabled = true, \
+    .dualsid = false, \
+    .chiptype = 0x0,  /* real */ \
+    .clonetype = 0x0, /* disabled */ \
+    .sid1type = 0x0,  /* unknown */ \
+    .sid2type = 0x0,  /* unknown */ \
+  }, \
+  .socketTwo = { \
+    .enabled = true, \
+    .dualsid = false, \
+    .act_as_one = false, \
+    .chiptype = 0x0,  /* real */ \
+    .clonetype = 0x0, /* disabled */ \
+    .sid1type = 0x0,  /* unknown */ \
+    .sid2type = 0x0,  /* unknown */ \
+  }, \
+  .LED = { \
+    .enabled = true, \
+    .idle_breathe = true, \
+  }, \
+  .RGBLED = { \
+    .enabled = true, \
+    .idle_breathe = true, \
+    .brightness = 0x7F,  /* Half of max brightness or disabled if no RGB LED */ \
+    .sid_to_use = 1, \
+  }, \
+  .Cdc = { \
+    .enabled = true \
+  }, \
+  .WebUSB = { \
+    .enabled = true \
+  }, \
+  .Asid = { \
+    .enabled = true \
+  }, \
+  .Midi = { \
+    .enabled = true \
+  }, \
+} \
+
