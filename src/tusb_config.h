d02f492fb1ac9cb42f1cf7236ac41cd65f2873e2
loudnl <LouDnl@users.noreply.github.com>
2025-04-12 20:48:20 +0200

    Fix CDC fifo at 64 bytes (max)

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -115,15 +115,15 @@
 #define CFG_TUD_VENDOR           1
 
 // CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE    (TUD_OPT_HIGH_SPEED ? 512 : 64)  // Even at 512KB only 64KB will be used
+#define CFG_TUD_CDC_EP_BUFSIZE    64  // Even at 512KB only 64KB will be used because of TUD_OPT_FULL_SPEED, Pico is only USB2.0
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE    (TUD_OPT_HIGH_SPEED ? 512 : 64)  // Even at 512KB only 64KB will be used
-#define CFG_TUD_CDC_TX_BUFSIZE    (TUD_OPT_HIGH_SPEED ? 512 : 64)  // Even at 512KB only 64KB will be used
+#define CFG_TUD_CDC_RX_BUFSIZE    64  // Even at 512KB only 64KB will be used because of TUD_OPT_FULL_SPEED, Pico is only USB2.0
+#define CFG_TUD_CDC_TX_BUFSIZE    64  // Even at 512KB only 64KB will be used because of TUD_OPT_FULL_SPEED, Pico is only USB2.0
 
 // MIDI FIFO size of TX and RX
-#define CFG_TUD_MIDI_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)  // Even at 512KB only 64KB will be used
-#define CFG_TUD_MIDI_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)  // Even at 512KB only 64KB will be used
+#define CFG_TUD_MIDI_RX_BUFSIZE   64  // Even at 512KB only 64KB will be used because of TUD_OPT_FULL_SPEED, Pico is only USB2.0
+#define CFG_TUD_MIDI_TX_BUFSIZE   64  // Even at 512KB only 64KB will be used because of TUD_OPT_FULL_SPEED, Pico is only USB2.0
 
 // Vendor FIFO size of TX and RX
 // If not configured vendor endpoints will not be buffered
